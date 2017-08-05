85280b654ef5673906645558dcf57962d90de35c
hongil.jo <hongil.jo@navercorp.com>
2017-08-05 17:20:17 +0900

    add spring example1

---
+++ boardservice/src/main/java/com/springbook/biz/board/BoardServiceImpl.java
@@ -0,0 +1,44 @@
+package com.springbook.biz.board;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+	
+	@Autowired
+	private BoardDAO boardDao;
+	
+	@Override
+	public void insertBoard(BoardDTO dto) {
+		// TODO Auto-generated method stub
+		boardDao.insertBoard(dto);
+	}
+
+	@Override
+	public void updateBoard(BoardDTO dto) {
+		// TODO Auto-generated method stub
+		boardDao.updateBoard(dto);
+	}
+
+	@Override
+	public void deleteBoard(BoardDTO dto) {
+		// TODO Auto-generated method stub
+		boardDao.deleteBoard(dto);
+	}
+
+	@Override
+	public BoardDTO getBoard(BoardDTO dto) {
+		// TODO Auto-generated method stub
+		return boardDao.getBoard(dto);
+	}
+
+	@Override
+	public List<BoardDTO> getBoardList(BoardDTO dto) {
+		// TODO Auto-generated method stub
+		return boardDao.getBoardList(dto);
+	}
+
+}
